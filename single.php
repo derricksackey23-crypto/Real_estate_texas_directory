e5df61c2856a2453f039827650fd64e42cf279c8
derricksackey23-crypto <derricksackey23@gmail.com>
2026-05-11 20:42:29 +0000

    Update single.php

--- single.php
+++ single.php
@@ -1,39 +1,26 @@
 <?php
 /**
- * Single Post Template (Fallback)
+ * Single Post Template
  */
+
 get_header();
 ?>
 
 <main class="site-main">
-    <div class="container">
-        <?php while (have_posts()): the_post(); ?>
-            <article class="post-content">
-                <?php retex_breadcrumbs(); ?>
-                
-                <header class="single-post-header">
+    <?php while (have_posts()) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <div class="container">
+                <header class="entry-header">
                     <h1><?php the_title(); ?></h1>
-                    <p class="card-meta">
-                        By <?php the_author(); ?> | 
-                        <time datetime="<?php echo esc_attr(get_the_date('c')); ?>">
-                            <?php echo get_the_date('F j, Y'); ?>
-                        </time>
-                    </p>
                 </header>
-                
-                <?php if (has_post_thumbnail()): ?>
-                    <figure class="post-featured-image">
-                        <img src="<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'large')); ?>" 
-                             alt="<?php the_title_attribute(); ?>">
-                    </figure>
-                <?php endif; ?>
-                
-                <div class="post-section">
+
+                <div class="entry-content">
                     <?php the_content(); ?>
                 </div>
-            </article>
-        <?php endwhile; ?>
-    </div>
+            </div>
+        </article>
+    <?php endwhile; ?>
 </main>
 
-<?php get_footer(); ?>
+<?php
+get_footer();
