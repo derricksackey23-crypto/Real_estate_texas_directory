8d40aeb30960a28b6d013d75f2dbe9724c39b0cc
derricksackey23-crypto <derricksackey23@gmail.com>
2026-05-11 20:41:56 +0000

    Update page.php

--- page.php
+++ page.php
@@ -2,32 +2,23 @@
 /**
  * Page Template
  */
+
 get_header();
 ?>
 
 <main class="site-main">
-    <div class="container">
-        <?php while (have_posts()): the_post(); ?>
-            <article class="page-content">
-                <?php retex_breadcrumbs(); ?>
-                
-                <header class="single-post-header">
-                    <h1><?php the_title(); ?></h1>
-                </header>
-                
-                <?php if (has_post_thumbnail()): ?>
-                    <figure class="post-featured-image">
-                        <img src="<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'large')); ?>" 
-                             alt="<?php the_title_attribute(); ?>">
-                    </figure>
-                <?php endif; ?>
-                
-                <div class="post-section">
-                    <?php the_content(); ?>
-                </div>
-            </article>
-        <?php endwhile; ?>
-    </div>
+    <?php while (have_posts()) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <div class="page-header">
+                <h1><?php the_title(); ?></h1>
+            </div>
+
+            <div class="page-content container">
+                <?php the_content(); ?>
+            </div>
+        </article>
+    <?php endwhile; ?>
 </main>
 
-<?php get_footer(); ?>
+<?php
+get_footer();
